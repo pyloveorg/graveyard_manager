ee7834219214e73bf84521351aca5fe63a9f6063
Dawid Żurawski <zrk1992@gmail.com>
2018-05-12 21:08:52 +0200

    poprawa bezpieczenstwa systemu odzyskiwania hasła

--- views_login_system.py
+++ views_login_system.py
@@ -12,7 +12,6 @@ from itsdangerous import URLSafeTimedSerializer, SignatureExpired, BadSignature
 from config import APP
 from data_validate import EmailForm, LoginForm, DataForm, PwForm, is_safe_next
 from data_db_manage import register_new_user, change_user_data, change_user_pw
-from data_func_manage import generate_password
 from db_models import db, User
 from mail_sending import common_msg
 
@@ -83,7 +82,7 @@ def password_recovery():
     return render_template('recovery.html', form_email=form_email)
 
 
-@pages_log_sys.route('/pw_recovery/<pw_token>')
+@pages_log_sys.route('/pw_recovery/<pw_token>', methods=['GET', 'POST'])
 def recovery_password(pw_token):
     """Strona do generowania nowego hasła.
 
@@ -93,14 +92,14 @@ def recovery_password(pw_token):
     try:
         token_id = temp_serializer.loads(pw_token, salt='pw-recovery', max_age=3600)
         user = User.query.filter_by(token_id=token_id).first()
-        # funkcja generująca hasło z modułu data_func_manage
-        new_pw = generate_password()
-        change_user_pw(user, new_pw, form=False)
-        db.session.commit()
-        common_msg('Nowe hasło', user.email, 'new_password', new_pw)
-        flash('Nowe hasło zostało wysłane na twój e-mail.', 'succes')
-        return redirect(url_for('pages_log_sys.login'))
-    # wyjątek gdy link jest zmodyfikowany, wykorzystany lub przestarzały
+        if user.email:
+            form_pw = PwForm(request.form)
+            if request.method == 'POST' and form_pw.validate():
+                change_user_pw(user, form_pw)
+                db.session.commit()
+                flash('Hasło zostało zmienione', 'succes')
+                return redirect(url_for('pages_log_sys.login'))
+            return render_template('user_settings.html', form_pw=form_pw)
     except (SignatureExpired, BadSignature, AttributeError):
         flash('Podany link jest nieaktywny!', 'error')
         return redirect(url_for('pages.index'))
